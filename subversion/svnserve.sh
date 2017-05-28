8cf00181fae4c7cfa6a66854926d620a021801cb
svon <svon@svon.org>
2017-05-29 00:49:24 +0800

    svn

--- subversion/svnserve.sh
+++ subversion/svnserve.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/svnserve -d -r "/opt/svn/data" --config-file "/opt/svn/config/svnserve.conf" --listen-port 3690
+/usr/bin/svnserve -r "/opt/svn/data" --config-file "/opt/svn/config/svnserve.conf" --listen-port 3690
