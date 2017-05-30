3cf6e45aefe3af2c5dbf9ee4b36a80ab8fa076bf
svon <svon@svon.org>
2017-05-30 16:17:02 +0800

    django

--- django/uwsgi.sh
+++
@@ -1,4 +0,0 @@
-
-/etc/init.d/nginx restart
-
-uwsgi --http :8000 --chdir /home/api_svon/ --wsgi-file api_svon/wsgi.py --master --processes 4 --threads 2
--- subversion/svnserve.sh
+++ subversion/svnserve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/svnserve -d -r /opt/svn/data --config-file /opt/svn/config/svnserve.conf --listen-port 3690
+/usr/bin/svnserve -d -r /opt/svn/data --config-file /opt/svn/config/svnserve.conf
 
 /opt/svn/data
 python -m SimpleHTTPServer 8080
